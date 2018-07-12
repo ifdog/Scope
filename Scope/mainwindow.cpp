44b57a5a8dfc369903693113ca210d6c054d4fbf
ifdog <ifdog@outlook.com>
2018-07-12 09:26:56 +0800

    adjust ui

--- Scope/mainwindow.cpp
+++ Scope/mainwindow.cpp
@@ -1,15 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <Windows.h>
+#include "QShortcut"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_S),this,SLOT(close()));
     setWindowFlags(Qt::WindowStaysOnTopHint|Qt::Dialog);
     SetWindowLongA(HWND(winId()),-20,0x80000);
     SetLayeredWindowAttributes(HWND(winId()),65280,255,1);
+    ui->lb_content->setStyleSheet("QLabel {background:#00FF00}");
 }
 
 MainWindow::~MainWindow()
